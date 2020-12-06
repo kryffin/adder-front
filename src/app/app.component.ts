96962439f70a4a2ecc7050107c7daea9906c025f
nicolasklz <nicolaskleinhentz@gmail.com>
2020-12-06 14:53:51 +0100

    changed the way the numbers are shown and added a dynamic behavior to the divide button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,12 @@ export class AppComponent {
   title = 'adder-front';
 
   private _cur: number;
+  private _temp: number;
   private _rep: number;
 
   constructor(private _http: HttpClient) {
     this._cur = 0;
+    this._temp = 0;
     this._rep = 0;
   }
 
@@ -23,6 +25,23 @@ export class AppComponent {
 
   get cur(): number {
     return this._cur;
+  }
+
+  // for some reason {{cur.toFixed(2)}} worked but gave a console error
+  // for some reason {{cur | number: 2}} did not work, says cur is a string but no idea why
+  // so made this method to manually limit to 2 the number of decimals
+  fix(num: number): string {
+    return num.toFixed(2);
+  }
+
+  // dynamically changes the temp variable in order to prevent divison by zero with checkTempNonZero
+  changeTemp(num: string): void {
+    this._temp = Number.parseFloat(num);
+  }
+
+  // used to dynamically disable the divide button if temp = 0
+  checkTempNonZero(): boolean {
+    return this._temp != 0.;
   }
 
   private httpOptions = {
